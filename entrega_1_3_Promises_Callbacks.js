7e77e395904a36e1444f2a00e81822427471d2bb
claremcc <mccarron.cl@gmail.com>
2022-03-16 00:07:36 +0100

    Attempt 1 - entrega 1.3

---
+++ entrega_1_3_Promises_Callbacks.js
@@ -0,0 +1,90 @@
+// Nivell 1 - Exercici 1: Crea una funció que retorni una Promise que invoqui la funció resolve() o reject() que rep. Invoca-la passant-li les dues funcions de manera que imprimeixin un missatge diferent depenent de si la Promise es resol o no
+let promiseResolved1 = false;
+function returnPromise (_params) {
+    return new Promise(function(resolve, reject) {
+        if (_params) {
+            resolve("This is resolved")
+        } else {
+            reject("This is rejected")
+        }
+    });
+}
+returnPromise(promiseResolved1)
+.then(message => console.log("Then: " + message))
+.catch(message => console.log("Catch: " + message))
+
+
+// Nivell 1 - Exercici 2: Crea una arrow function que rebi un paràmetre i una funció callback i li passi a la funció un missatge o un altre (que s'imprimirà per consola) en funció del paràmetre rebut.
+// no promise in this one
+let userDetails = {
+    age: 17,
+    name: "Jane"
+}
+const getMessage = (_param, callback) => {
+    let message = ``
+    if(_param.age >= 18){
+        message = `${_param.name} is allowed to vote` 
+    }else{
+        message = `${_param.name} is not allowed to vote` 
+    }
+    callback(message)
+}
+function displayMessage(_param){
+    console.log(_param)
+}
+getMessage(userDetails, displayMessage)
+
+
+// Nivell 2 - Exercici 1: Donats els objectes employees i salaries, crea una arrow function getEmployee que retorni una Promise efectuant la cerca en l'objecte pel seu id.
+let employees = [{
+    id: 1,
+    name: 'Linux Torvalds'
+}, {
+    id: 2,
+    name: 'Bill Gates'
+},{
+    id: 3,
+    name: 'Jeff Bezos'
+}];
+ 
+let salaries = [{
+    id: 1,
+    salary: 4000
+}, {
+    id: 2,
+    salary: 1000
+}, {
+    id: 3,
+    salary: 2000
+}];
+let employeeName = 'Bill Gates'; 
+const getEmployee = (_param) => {
+    return new Promise((resolve, reject) => {
+        for(let x in employees){
+            if(employees[x].name === _param){
+                resolve(employees[x])
+            }
+        }
+        reject(new Error("Employee not found. "))
+    })
+};
+// Nivell 2 - Exercici 2: Crea una altra arrow function getSalary que rebi com a paràmetre un objecte employee i retorni el seu salari.
+let getSalary = (employeeObj) => {
+    let found = false;
+    for(let x in salaries){
+        if(salaries[x].id === employeeObj.id){
+            found = true;
+            return salaries[x].salary
+        }
+    }
+    if(!found){
+        return "Not found. "
+    }
+};
+// Nivell 2 - Exercici 3: Invoca la primera funció getEmployee i després getSalary niant l'execució de les dues promises.
+getEmployee(employeeName)
+.then((employeeDetails) => {
+    console.log(`Then: Id Found. Salary is: ${getSalary(employeeDetails)}`);
+})
+.catch(error => console.log("Catch: " + error))
+// Nivell 3 - Exercici 1: Fixa un element catch a la invocació del nivell anterior que capturi qualsevol error i el mostri per la consola.
