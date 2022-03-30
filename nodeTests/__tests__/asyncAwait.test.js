472e4e1ce1e886f0bc004b3cfad0fe13a83c0680
claremcc <mccarron.cl@gmail.com>
2022-03-31 01:14:28 +0200

    N1 E1 tests completed

--- nodeTests/__tests__/asyncAwait.test.js
+++ nodeTests/__tests__/asyncAwait.test.js
@@ -1,10 +1,32 @@
 var myAsyncTest = require('../app/async_await_func');
 
-test('el nom de l empleat i el seu salari ', () => {
-  expect(myAsyncTest.findingNameAndSalary(2)).toBe(1000);
+test('Employee id found - resolved', () => {
+  return expect(myAsyncTest.getEmployee(2)).resolves.toStrictEqual({"id": 2, "name": "Bill Gates"});
 });
 
-test('resolve message després de 2 segons', () => {
-    expect(myAsyncTest.asyFunc2()).toBe('resolved after 2 seconds');
-  });
+test('Employee Salary is returned - resolved', () => {
+  return expect(myAsyncTest.getSalary({"id": 2, "name": "Bill Gates"})).resolves.toBe(1000);
+});
 
+test('Resolve message després de 2 segons.', () => {
+  return expect(myAsyncTest.myDelayFunc()).resolves.toBe('resolved after 2 seconds');
+});
+
+test('Invoca funció getEmployee, returns a promise', () => {
+  return myAsyncTest.getEmployee(2).then(employeeDetails => {
+    expect(employeeDetails).toStrictEqual({"id": 2, "name": "Bill Gates"});
+  });
+});
+
+test('Invoca funció getSalary, returns a promise', () => {
+  return myAsyncTest.getSalary({"id": 2, "name": "Bill Gates"}).then(salaryDetails => {
+    expect(salaryDetails).toBe(1000);
+  });
+});
+
+test('Resolve message després de 2 segons - using Jest Fake Timer', () => {
+  jest.useFakeTimers();
+  jest.spyOn(global, 'setTimeout');
+  myAsyncTest.myDelayFunc()
+  expect(setTimeout).toHaveBeenCalledTimes(1);
+});
