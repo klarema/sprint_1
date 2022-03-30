a2b09fecbb5ca0315822ad5e35eb027abe31a4e8
claremcc <mccarron.cl@gmail.com>
2022-03-31 01:19:42 +0200

    Add N2 tests

---
+++ nodeTests/app/person.js
@@ -0,0 +1,10 @@
+
+module.exports =  class PersonaA {
+    constructor (nom) {
+      this.nom = nom;
+    }
+    dirNom() { console.log(this.nom)}
+  }
+  // const persona1 = new PersonaA("Laura");
+  // persona1.dirNom()
+  
