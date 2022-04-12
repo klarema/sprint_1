cc7b009fe8566323406f07522117decf9bbc0f67
claremcc <mccarron.cl@gmail.com>
2022-04-12 21:34:22 +0200

    Correct abstact class instanciation exercise

--- entrega_1_2_v2_Classes_ArrowFunc.js
+++ entrega_1_2_v2_Classes_ArrowFunc.js
@@ -66,59 +66,25 @@ persona1.dirNom()
 //   }
 // }
 
-var Calzado = function () {
-  if(this.constructor === Calzado){
+function Calzado() {
+  this.material="material";
+  this.tacon="tacon";
   throw new Error("Calzado is an Abstract Class. Cannot be instantiated. ");
-  }
-};
-Calzado.prototype.material = function () {
-  return "leather and synthetic mix"
 }
-Calzado.prototype.tacon = function () {
-throw new Error ("method must be implemented. ")
+Calzado.prototype.madeOf = function() {
+  return "Made of: "+this.material;
 }
-Calzado.prototype.style = function () {
-throw new Error ("method must be implemented. ")
+Calzado.prototype.taconType = function() {
+  return "Tacon type: "+this.tacon;
 }
 
-var Botas = function() {  
-Calzado.apply(this, arguments);  
-};  
-Botas.prototype = Object.create(Calzado.prototype);  
-Botas.prototype.constructor = Botas;  
-Botas.prototype.tacon = function() {  
-return "bajo";  
-}  
-Botas.prototype.style = function() {  
-return "formal or casual" 
-}  
-
-var Zapato = function() {
-Calzado.apply(this, arguments);  
+function createCalzado(material, tacon){
+  this.material=material;
+  this.tacon=tacon;
 }
-Zapato.prototype = Object.create(Calzado.prototype);  
-Zapato.prototype.constructor = Zapato;  
-Zapato.prototype.tacon = function() {
-  return "alto";
-}
-Zapato.prototype.style = function() {
-  return "formal";
-}
+createCalzado.prototype=Object.create(Calzado.prototype);
 
-function createCalzado(objId, ObjectType){
-  if(ObjectType === "zapato"){
-    console.log("Create zapato, ID: "+ objId)
-    var objId = new Zapato()
-    console.log("Tacon: " + objId.tacon() + ", Style: " + objId.style() + ", " + objId.material())
-  }
-  else{
-    console.log("Create botas, ID: "+ objId)
-    var objId = new Botas()
-    console.log("Tacon: " + objId.tacon() + ", Style: " + objId.style() + ", " + objId.material())
-  }
-
-}
-createCalzado("Calzado_1", "zapato");
-createCalzado("Calzado_2", "botas");
-createCalzado("Calzado_3", "zapato");
-
+var botas = new createCalzado("leather", "alto");
+console.log("botas - "+ botas.madeOf() +", "+ botas.taconType())
+var sneakers = new createCalzado("synthetic", "bajo");
+console.log("sneakers - "+ sneakers.madeOf() +", "+ sneakers.taconType())
