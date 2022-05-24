b711e53169338c72ddc235ea597de357038597b6
claremcc <mccarron.cl@gmail.com>
2022-05-24 18:13:04 +0200

    Correction to instance create function on Abstact class

--- entrega_1_2_v2_Classes_ArrowFunc.js
+++ entrega_1_2_v2_Classes_ArrowFunc.js
@@ -1,90 +1,60 @@
-
-// Nivell 1 - Exercici 1: Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres 
+// Nivell 1 - Exercici 1: Mostra per la consola el resultat d'una arrow function autoinvocable que sumi dos nombres
 console.log(
-  ( () => {
+  (() => {
     let num1 = 10;
     let num2 = 5;
-    let sumDosNombres = num1 + num2
-    return sumDosNombres
-})()
-)
-
+    let sumDosNombres = num1 + num2;
+    return sumDosNombres;
+  })()
+);
 
 // Nivell 2 - Exercici 1: una arrow function que, rebent un paràmetre, retorni un objecte amb un atribut que tingui com a valor el paràmetre rebut
 let myfunction = (arg1) => {
-    let object1 = {};
-    object1.name = arg1;
-    return object1
+  let object1 = {};
+  object1.name = arg1;
+  return object1;
 };
-console.log(`N2 Ex1 - objecte.atribut valor: ${myfunction("Tomas").name}`)
-
+console.log(`N2 Ex1 - objecte.atribut valor: ${myfunction("Tomas").name}`);
 
 // Nivell 2 - Exercici 2: una classe Persona que rebi un paràmetre 'nom' al ser instanciada. La classe inclourà un mètode dirNom que imprimeixi per consola el paràmetre 'nom'. Invoca el mètode dirNom des de fora de la classe
 class PersonaA {
-    constructor (nom) {
-      this.nom = nom;
-    }
-    dirNom() { console.log(this.nom)}
+  constructor(nom) {
+    this.nom = nom;
+  }
+  dirNom() {
+    console.log(this.nom);
+  }
 }
 const persona1 = new PersonaA("Laura");
-persona1.dirNom()
-
+persona1.dirNom();
 
 // Nivell 3 - Exercici 1: una function creadora d'objectes que faci instàncies d'una classe abstracta. Invoca-la amb diferents definicions
-// class Calzado{
-//   constructor() {
-//     if(this.constructor === Calzado){
-//     throw new Error("Calzado is an Abstract Class. Cannot be instantiated. ");
-//     }
-//   }
-//   material(){
-//     return "Material: leather and synthetic mix"
-//   }
-//   tacon(){
-//     throw new Error ("method must be implemented. ")
-//   }
-//   style(){
-//     throw new Error ("method must be implemented. ")
-//   }
-
-// }
-// class Botas extends Calzado {
-//   tacon(){
-//     return "bajo"
-//   }
-//   style(){
-//     return "casual"
-//   }
-
-// }
-// class Zapato extends Calzado {
-//   tacon(){
-//     return "alto"
-//   }
-//   style(){
-//     return "formal"
-//   }
-// }
 
 function Calzado() {
-  this.material="material";
-  this.tacon="tacon";
+  this.material = "material";
+  this.tacon = "tacon";
   throw new Error("Calzado is an Abstract Class. Cannot be instantiated. ");
 }
-Calzado.prototype.madeOf = function() {
-  return "Made of: "+this.material;
-}
-Calzado.prototype.taconType = function() {
-  return "Tacon type: "+this.tacon;
+Calzado.prototype.madeOf = function () {
+  return "Made of: " + this.material;
+};
+Calzado.prototype.taconType = function () {
+  return "Tacon type: " + this.tacon;
+};
+
+function createCalzado(material, tacon) {
+  let newInstance = Object.create(Calzado.prototype);
+  newInstance.madeOf = material;
+  newInstance.taconType = tacon;
+  return newInstance;
 }
 
-function createCalzado(material, tacon){
-  this.material=material;
-  this.tacon=tacon;
-}
-createCalzado.prototype=Object.create(Calzado.prototype);
+var botas = createCalzado("leather", "alto");
+console.log(botas instanceof Calzado); // true
+console.log(botas.madeOf);
+console.log(botas.taconType);
 
-var botas = new createCalzado("leather", "alto");
-console.log("botas - "+ botas.madeOf() +", "+ botas.taconType())
-var sneakers = new createCalzado("synthetic", "bajo");
-console.log("sneakers - "+ sneakers.madeOf() +", "+ sneakers.taconType())
+var sneakers = createCalzado("synthetic", "bajo");
+console.log(sneakers instanceof Calzado); // true
+console.log(sneakers.madeOf);
+console.log(sneakers.taconType);
